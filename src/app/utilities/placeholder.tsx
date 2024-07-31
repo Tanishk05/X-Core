83ddbd5061d3ddc04690c644514c0f7185d06079
Tanishk <tanishkkumarshrivastava@gmail.com>
2024-07-31 20:37:11 +0530

    Reduce the code of department page

---
+++ src/app/utilities/placeholder.tsx
@@ -0,0 +1,40 @@
+const department = [
+    {
+        name: "AIML",
+        lock: false
+    },
+    {
+        name: "CSCY",
+        lock: false
+    },
+    {
+        name: "AIDS",
+        lock: true
+    },
+    {
+        name: "EX",
+        lock: true
+    },
+    {
+        name: "CS",
+        lock: true
+    },
+    {
+        name: "EC",
+        lock: true
+    },
+    {
+        name: "MECH.",
+        lock: true
+    },
+    {
+        name: "CIVIL",
+        lock: true
+    },
+    {
+        name: "CSIT",
+        lock: true
+    },
+]
+
+export { department };
